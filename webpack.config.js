a957831f6f2b6c02184a3364a00d6a690f13371f
Ibwedagama <wardana.wedagama@gmail.com>
2021-06-09 22:31:08 +0700

    see what happen when using entry and output on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,29 @@
+// ?? karena menggunakan node.js maka harus dilakukan require("path")
+// ?? "path" sendiri merupakan module bawaaan dari node.js
+const path = require("path")
+
 // webpack harus dibungkus dengan expression `module.exports`
 module.exports = {
   // ?? development mode membuat hasil bundle dari webpack berisi informasi - informasi lebih
   // ?? selain `development` mode, terdapat pula mode `production` yang akan membuat bundle lebih kecil
   mode: "development",
+
+
+  // ?? entry adalah file js utama yang akan di bundle oleh webpack, path nya relatif terhadap posisi `webpack.config.js`
+  entry: './src/index.js',
+
+  // ?? output adalah keluaran dari hasil bundle yang dilakukan oleh webpack
+  output: {
+
+    // ?? `filename` merujuk kepada nama file yang akan dibuat oleh webpack sebagai hasil bundle
+    filename: 'bundle.js',
+
+    // ?? `path` adalah lokasi dimana file hasil bundle akan ditaruh
+    // ?? karena berjalan di node.js maka harus menggunakan ekspresi `path.resolve`
+    // ?? untuk melakukan output, kita harus import (require()) package `path` ini diawal
+    path: path.resolve(__dirname, 'public')
+  },
+
   module: {
     // didalam objek module, terdapat sebuah array rules yang berisi objek - objek
     rules: [
